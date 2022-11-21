257a883090593cfa6df8631f8afd5c80115a1223
Naga Shiva Krishna Surapaneni <63412856+Snskdevops@users.noreply.github.com>
2022-11-22 05:19:44 +1300

    Update main.tf
    added disks

--- VMware/terraform/hcl/singleVM/main.tf
+++ VMware/terraform/hcl/singleVM/main.tf
@@ -194,10 +194,25 @@ resource "vsphere_virtual_machine" "vm_1" {
   }
 
   disk {
-    label          = "${var.vm_1_name}0.vmdk"
-    size           = var.vm_1_root_disk_size
-    keep_on_remove = var.vm_1_root_disk_keep_on_remove
-    datastore_id   = data.vsphere_datastore.vm_1_datastore.id
+    label          = "${var.vm_name}.vmdk"
+    size           = var.vm_disk_size
+    keep_on_remove = var.vm_disk_keep_on_remove
+    datastore_id   = data.vsphere_datastore.datastore.id
+    unit_number    = 0
+  }
+  disk {
+    label          = "${var.vm_name}1.vmdk"
+    size           = var.vm_disk_size
+    keep_on_remove = var.vm_disk_keep_on_remove
+    datastore_id   = data.vsphere_datastore.datastore.id
+    unit_number    = 1
+  }
+  disk {
+    label          = "${var.vm_name}2.vmdk"
+    size           = var.vm_disk_size
+    keep_on_remove = var.vm_disk_keep_on_remove
+    datastore_id   = data.vsphere_datastore.datastore.id
+    unit_number    = 2
   }
 }
 
